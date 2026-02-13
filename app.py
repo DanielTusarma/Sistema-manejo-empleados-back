a2f5b8ea04461141048553e6d362296629a3414f
DanielTusarma <daniele.tusarmag@uqvirtual.edu.co>
2026-02-12 23:56:12 -0500

    Se agregan modelo Empleado, schemas y modelos

---
+++ app.py
@@ -0,0 +1,41 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from flask import Flask
+from flask_cors import CORS
+
+from extensions import db, migrate, ma
+from api import bp as api_bp
+import api.empleados # Importar el módulo para registrar los recursos de empleados
+import models # Importar el módulo para registrar los modelos con SQLAlchemy asegurar que estén disponibles para las migraciones
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # Configuración de la base de datos desde variables de entorno
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JSON_AS_ASCII"] = False # Para soportar caracteres UTF-8 en JSON
+    
+    # Inicializar extensiones
+    db.init_app(app)
+    migrate.init_app(app, db)
+    ma.init_app(app)
+    
+    # Habilitar CORS para la API
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
+    
+    # Registrar blueprint de la API
+    app.register_blueprint(api_bp)
+    
+    @app.get("/")
+    def home():
+        return "Api de Empleados"
+    
+    return app
+
+if __name__ == "__main__":
+    create_app().run(debug=os.getenv("FLASK_ENV") == "development")
---
+++ schemas.py
@@ -0,0 +1,17 @@
+from extensions import ma, db
+from marshmallow import fields, validate
+from models import Empleado
+
+class EmpleadoSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Empleado
+        load_instance = True
+        sqla_session = db.session
+        
+    idEmpleado = ma.auto_field(dump_only=True)
+    nombre = ma.auto_field(required=True, validate=validate.Length(min=1, max=100))
+    departamento = ma.auto_field(required=True, validate=validate.Length(min=1, max=100))
+    sueldo = fields.Float(required=True, validate=validate.Range(min=0))
+    
+empleado_schema = EmpleadoSchema()
+empleados_schema = EmpleadoSchema(many=True)
